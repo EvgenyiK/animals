cc66ffead616ddeae64d7c578c1e575656c2e76a
Evgen <evgenprogrammer@gmail.com>
2020-08-24 19:05:17 +0300

    jwt auth

--- server/app/auth.go
+++ server/app/auth.go
@@ -1,35 +1,77 @@
 package app
 
-import ("net/http"
-u "github.com/EvgenyiK/animals/utils/utils"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/EvgenyiK/animals/server/models"
+	u "github.com/EvgenyiK/animals/server/utils"
+	jwt "github.com/dgrijalva/jwt-go"
 )
 
-
-
-
-var JwtAuthentication = func (next http.Handler) http.Handler {
-	return http.HandlerFunc(func (w http.ResponseWriter, r *http.Request)  {
-		notAuth:= []string{"api/user/new", "api/user/login"}//List of endpoints that doesn't require auth
-		requestPath:= r.URL.Path //current request path
-		 //check if request does not need authentication, serve the request if it doesn't need it
+var JwtAuthentication = func(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		notAuth := []string{"api/user/new", "api/user/login"} //List of endpoints that doesn't require auth
+		requestPath := r.URL.Path                             //current request path
+		//check if request does not need authentication, serve the request if it doesn't need it
 
 		for _, value := range notAuth {
 			if value == requestPath {
-				next.ServeHTTP(w,r)
+				next.ServeHTTP(w, r)
 				return
 			}
 		}
-		
-		response := make(map[string] interface{})
-		tokenHeader:= r.Header.Get("Autorization") //Grab the token from the header
 
-		if tokenheader == "" {//Token is missing, returns with error code 403 Unauthorized
+		response := make(map[string]interface{})
+		tokenHeader := r.Header.Get("Autorization") //Grab the token from the header
+
+		if tokenHeader == "" { //Token is missing, returns with error code 403 Unauthorized
 			response = u.Message(false, "Missing auth token")
 			w.WriteHeader(http.StatusForbidden)
-			w.Header().Add("Content-Type","application/json")
+			w.Header().Add("Content-Type", "application/json")
 			u.Responde(w, response)
 			return
 		}
 
-	})
+		splited:=strings.Split(tokenHeader, "")//The token normally comes in format `Bearer {token-body}`, we check if the retrieved token matched this requirement
+		if len(splited)!=2 {
+			response = u.Message(false, "Invalid/Malformed and token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Responde(w,response)
+			return
+		}
+
+		tokenPart:= splitted[1] //Grab the token part, what we are truly interested in
+		tk:= &models.Token{}
+
+		token, err:= jwt.ParseWithClaims(tokenPart, tk, func (token *jwt.Token) (interface{}, error) {
+			return []byte(os.Getenv("token_password")), nil
+		})
+
+		if err != nil {//Malformed token, returns with http code 403 as usual
+			response = u.Message(false, "Malformed authentication token")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Responde(w,response)
+			return
+		}
+
+		if !token.Valid {//Token is invalid, maybe not signed on this server
+			response:= u.Message(false,"Token is not valid.")
+			w.WriteHeader(http.StatusForbidden)
+			w.Header().Add("Content-Type", "application/json")
+			u.Responde(w,response)
+			return
+		}
+
+		//Everything went well, proceed with the request and set the caller to the user retrieved from the parsed token
+		fmt.Sprintf("Use %", tk.Username)  //Useful for monitoring
+		ctx:= context.WithValue(r.Context(), "user", tk.UserId)
+		r = r.WithContext(ctx)
+		next.ServeHTTP(w,r)//proceed in the middleware chain!
+	});
 }
--- server/go.mod
+++ server/go.mod
@@ -1,4 +1,6 @@
-module github.com/EvgenyiK/animals
+module github.com/EvgenyiK/animals/server
 
 go 1.15
 
+
+replace github.com/EvgenyiK/animals/server => /home/evgen/development/animals/server
